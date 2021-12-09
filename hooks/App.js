b68475123e42b09783b3c187a0874c28ada1feff
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-08 21:51:10 -0600

    Completed

---
+++ hooks/App.js
@@ -0,0 +1,13 @@
+import ProductInfo from './ProductInfo';
+import { UserInfo } from './UserInfo';
+
+const App = () => {
+	return (
+		<div>
+			<UserInfo />
+			<p>Testing</p>
+		</div>
+	);
+};
+
+export default App;
